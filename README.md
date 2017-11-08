85cc73457f7a6c26b671a38dbe50085e3aaa8e09
Jack Clarke <tokyojack2001@yahoo.com>
2017-11-08 05:53:55 +0000

    Update README.md

--- README.md
+++ README.md
@@ -6,7 +6,56 @@ INSTRUCTIONS:
 2. Change the package at the top to your needed one, and you're good to go!
 
 TO USE:
-1. Check out the test/Test.java file for an example.
+
+Here's a quick example of it with Vault. (Just quick example)
+```
+public class Test extends JavaPlugin {
+
+	private Economy economy;
+
+	// Plugin name, does disable the plugin if not found, and the JavaPlugin
+	// instance.
+	Gependecy vaultDependecy = new Gependecy("Vault", true, this) {
+
+		// Code that runs if "Vault" is found
+		@Override
+		public void ifFound() {
+			setupEconomy();
+		}
+
+		// Code that runs if "Vault" is NOT found
+		@Override
+		public void ifNotFound() {
+			// Nothing happens.
+		}
+	};
+
+	public void onEnable() {
+		// Checks if the plugin when the plugin is enabled
+		vaultDependecy.check();
+	}
+
+	public double getBalance(Player player) {
+		if (!this.vaultDependecy.isFound()) {
+			player.sendMessage("Cannot get your balance as 'Vault' is not available.");
+			return -1.0; // -1 Means nothing, as 0 can be a players balance.
+		}
+
+		return this.economy.getBalance(player); // Returns the player's balance
+	}
+
+	// From Vault's WIKI if I'm correct
+	private boolean setupEconomy() {
+		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager()
+				.getRegistration(net.milkbowl.vault.economy.Economy.class);
+		if (economyProvider != null)
+			economy = economyProvider.getProvider();
+
+		return (economy != null);
+	}
+
+}
+```
 
 Now you've just install the program! You're a pretty cool person:
 1. ```(•_•)```
--- test/Test.java
+++ test/Test.java
@@ -1,35 +1,54 @@
 package packagename;
 
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.economy.Economy;
 
 public class Test extends JavaPlugin {
 
-	//Plugin name, does disable the plugin if not found, and the JavaPlugin instance.
+	private Economy economy;
+
+	// Plugin name, does disable the plugin if not found, and the JavaPlugin
+	// instance.
 	Gependecy vaultDependecy = new Gependecy("Vault", true, this) {
 
+		// Code that runs if "Vault" is found
 		@Override
 		public void ifFound() {
-			// Code that runs if found
+			setupEconomy();
 		}
 
+		// Code that runs if "Vault" is NOT found
 		@Override
 		public void ifNotFound() {
-			// Code that runs if NOT found
+			// Nothing happens.
 		}
 	};
 
 	public void onEnable() {
-		//Checks if the plugin when the plugin is enabled
+		// Checks if the plugin when the plugin is enabled
 		vaultDependecy.check();
 	}
 
-	public void takeMoney(Player player) {
-		if (this.vaultDependecy.isFound()) {
-			// Take money
-		} else {
-			player.sendMessage("That plugin is not found :(");
+	public double getBalance(Player player) {
+		if (!this.vaultDependecy.isFound()) {
+			player.sendMessage("Cannot get your balance as 'Vault' is not available.");
+			return -1.0; // -1 Means nothing, as 0 can be a players balance.
 		}
+
+		return this.economy.getBalance(player); // Returns the player's balance
+	}
+
+	// From Vault's WIKI if I'm correct
+	private boolean setupEconomy() {
+		RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager()
+				.getRegistration(net.milkbowl.vault.economy.Economy.class);
+		if (economyProvider != null)
+			economy = economyProvider.getProvider();
+
+		return (economy != null);
 	}
 
 }
