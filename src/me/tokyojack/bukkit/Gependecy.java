7600b662af42c3496efed00442c3816c4cf4fb51
Jack Clarke <tokyojack2001@yahoo.com>
2017-11-08 03:14:13 +0000

    Add new features.

--- src/me/tokyojack/bukkit/Gependecy.java
+++ src/me/tokyojack/bukkit/Gependecy.java
@@ -6,41 +6,48 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class Gependecy {
 
-	private String pluginName;
+	private String dependecyPluginName;
 	private JavaPlugin javaPlugin;
+	private boolean isRequired;
 
-	public Gependecy(String pluginName, JavaPlugin javaPlugin) {
-		this.pluginName = pluginName;
+	private boolean isFound;
+
+	public Gependecy(String dependecyPluginName, boolean isRequired, JavaPlugin javaPlugin) {
+		this.dependecyPluginName = dependecyPluginName;
 		this.javaPlugin = javaPlugin;
+		this.isRequired = isRequired;
+
+		this.isFound = false;
 	}
 
 	public abstract void ifFound();
-	// public abstract void ifNotFound();
 
-	//Making a "check" function as it's much more easier to understand.
-	//As oppose to adding it in the constructor
+	public abstract void ifNotFound();
+
 	public void check() {
-		if (Bukkit.getPluginManager().getPlugin(this.pluginName) == null
-				|| !Bukkit.getPluginManager().getPlugin(this.pluginName).isEnabled()) {
-			logToConsole(ChatColor.RED + "The plugin '" + this.pluginName + "' is not found. Plugin is disabeling.");
-			Bukkit.getPluginManager().disablePlugin(javaPlugin);
-			// ifNotFound();
+		if (Bukkit.getPluginManager().getPlugin(this.dependecyPluginName) == null
+				|| !Bukkit.getPluginManager().getPlugin(this.dependecyPluginName).isEnabled()) {
+
+			if (isRequired)
+				Bukkit.getPluginManager().disablePlugin(this.javaPlugin);
+
+			String finalText = isRequired ? "Plugin is disabeling" : "Luckily, that plugin is optional";
+			logToConsole(
+					ChatColor.RED + "The plugin '" + this.dependecyPluginName + "' is not found. " + finalText + ".");
+
+			ifNotFound();
 		} else {
-			logToConsole(ChatColor.GREEN + "The plugin '" + this.pluginName + "' has been found.");
+			logToConsole(ChatColor.GREEN + "The plugin '" + this.dependecyPluginName + "' has been found.");
 			ifFound();
 		}
 	}
 
-	public String getPluginName() {
-		return this.pluginName;
-	}
-
-	public JavaPlugin getJavaPlugin() {
-		return this.javaPlugin;
+	public boolean isFound() {
+		return this.isFound;
 	}
 
 	private void logToConsole(String message) {
-		final String PREFIX = ChatColor.AQUA + "[Gependecy]";
+		final String PREFIX = ChatColor.AQUA + "[Gependecy] [" + this.javaPlugin.getDescription().getName() + "]";
 		Bukkit.getServer().getConsoleSender().sendMessage(PREFIX + " " + message);
 	}
 }
--- test/Test.java
+++ test/Test.java
@@ -1,21 +1,35 @@
-package me.tokyojack.mcmarket.util;
+package packagename;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Test extends JavaPlugin {
 
-	private PluginApi pluginApi;
+	//Plugin name, does disable the plugin if not found, and the JavaPlugin instance.
+	Gependecy vaultDependecy = new Gependecy("Vault", true, this) {
+
+		@Override
+		public void ifFound() {
+			// Code that runs if found
+		}
+
+		@Override
+		public void ifNotFound() {
+			// Code that runs if NOT found
+		}
+	};
 
 	public void onEnable() {
-		
-		new Gependecy("PluginApi", this) {
-
-			@Override
-			public void ifFound() {
-				//Example
-				this.pluginAPI = PluginAPI.getInstance();
-			}
-		}.check();
-
+		//Checks if the plugin when the plugin is enabled
+		vaultDependecy.check();
 	}
+
+	public void takeMoney(Player player) {
+		if (this.vaultDependecy.isFound()) {
+			// Take money
+		} else {
+			player.sendMessage("That plugin is not found :(");
+		}
+	}
+
 }
